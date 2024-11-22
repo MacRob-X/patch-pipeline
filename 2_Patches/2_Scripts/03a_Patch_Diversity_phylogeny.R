33fc226952c5f326d3efba6ce2a07cfbe1e83e8c
MacRob-X <robert-macdonald@live.co.uk>
2024-11-22 11:35:12 +0000

    Add within-taxon diversity calculation
    Add calculation and plotting on phylogeny of colour diversity within taxon subgroups

--- 2_Patches/2_Scripts/03a_Patch_Diversity_phylogeny.R
+++ 2_Patches/2_Scripts/03a_Patch_Diversity_phylogeny.R
@@ -6,6 +6,7 @@
 rm(list=ls())
 
 # load libraries
+library(dplyr)
 library(ape)
 library(dispRity)
 library(umap)
@@ -19,10 +20,36 @@ library(ggplot2)
 # Load data ----
 
 # load patch data (PCA of whichever colourspace - generated in 02_Patch_Analyse_features.R)
-pcaAll <- readRDS("./2_Patches/3_OutputData/2_PCA_ColourPattern_spaces/Neoaves.patches.231030.PCAcolspaces.jndxyzlumr.240404.rds")
+pca_all <- readRDS(
+  here::here(
+    "2_Patches", "3_OutputData", "2_PCA_ColourPattern_spaces", "1_Raw_PCA",
+    "Neoaves.patches.231030.PCAcolspaces.jndxyzlum.240603.rds"
+  )
+)
 
 # load first 100 trees of Hackett backbone full trees
-phy <- read.tree("./4_SharedInputData/First100_AllBirdsHackett1.tre")
+phy <- ape::read.tree(
+  here::here(
+    "4_SharedInputData", "First100_AllBirdsHackett1.tre"
+  )
+)
+
+# load taxonomy and rename columns
+taxo <- read.csv(
+  here::here(
+    "4_SharedInputData", "BLIOCPhyloMasterTax_2019_10_28.csv"
+  ),
+  strings = F
+) %>% 
+  rename(
+    genus = GenusName,
+    family = BLFamilyLatin,
+    order = IOCOrder,
+    taxon_subgroup = Taxon_subgroup
+  ) %>% 
+  select(
+    TipLabel, genus, family, order, taxon_subgroup
+  )
 
 
 ################ SHOULD I TRIM TO MALES/FEMALES ONLY BEFORE I CALCULATE DISTANCE TO CENTROID? ##################
@@ -319,3 +346,260 @@ p2 <- ggplot(summaryData, aes(x = median_centroid_distance, y = median_NN_distan
  geom_text(aes(label = Taxon_subgroup), vjust = -0.5, hjust = -0.05) +   # adds labels to the plot itself
   theme_bw()
 p2
+
+
+# ------------------------------------------------------------------------------------------------------#
+# Calculate within-group diversity ----
+# mean distance to group centroid and mean pairwise distance
+
+# Specify taxonomic level to calculate at ("genus", "family", "order", "taxon_subgroup")
+tax_level <- "family"
+# Select sex to focus on ("M", "F", "All")
+sex_choice <- "M"
+## Drop depauperate clades? Specify min. number of species a clade must contain for it to be included
+## Minimum is 2, as can't calculate centroid distances or pairwise distances for groups with
+## only one species
+drop_depaup <- 2
+## Plot on log scale?
+log_choice <- TRUE
+
+
+
+# Convert PCA data to dataframe and append taxonomy
+pca_dat <- pca_all %>% 
+  magrittr::extract2("x") %>% 
+  as.data.frame() %>% 
+  mutate(
+    species = sapply(strsplit(rownames(.), split = "-"), "[", 1),
+    sex = sapply(strsplit(rownames(.), split = "-"), "[", 2)
+  ) %>% 
+  left_join(taxo, by = join_by("species" == "TipLabel"))
+
+# Filter by sex
+if(sex_choice != "All") {
+  pca_dat <- pca_dat %>% 
+    filter(
+      sex == sex_choice
+    )
+}
+
+# get clades with fewer species than specified minimum
+clades_to_keep <- pca_dat %>% 
+  count(get(tax_level)) %>% 
+  filter(
+    n >= drop_depaup
+  ) %>% 
+  magrittr::extract2(1)
+
+# remove these clades from data
+pca_dat <- pca_dat[pca_dat[, tax_level] %in% clades_to_keep, ]
+
+
+# Get list of unique taxa
+taxa <- pca_dat[, tax_level] %>% 
+  unique()
+
+
+# Create empty summary dataframe to populate with diversity metrics (for basic plots)
+summary_within_group_diversity <- data.frame(
+  taxa = taxa, 
+  mean_group_centr_dist = NA, 
+  mean_pairwise_dist = NA)
+colnames(summary_within_group_diversity) <- c(tax_level, "mean_group_centr_dist", "mean_pairwise_dist")
+
+# Create empty full species-level dataframe to populate with diversity metrics (for phylogenetic tree plots)
+within_group_diversity <- data.frame(
+  pca_dat[, c("species", "sex", tax_level)],
+  group_centr_dist = NA,
+  mean_group_centr_dist = NA,
+  mean_pairwise_dist = NA
+)
+
+# Loop over vector of unique taxa
+for (taxon in taxa) {
+  
+  # Get species data in taxon of interest in matrix form
+  dat <- pca_dat[pca_dat[, tax_level] == taxon, ]
+  rownames(dat) <- paste(dat$species, dat$sex, sep = "-")
+  mat <- as.matrix(dat[, 1:40])
+  
+  # Calculate distance of each species to within-group centroid and append to main dataframe
+  within_group_diversity$group_centr_dist[within_group_diversity[, tax_level] == taxon] <- dispRity::dispRity(
+    mat, metric = dispRity::centroids
+  )$disparity[[1]][[1]]
+  
+  # Calculate mean distance to within-group centroid and add to main dataframe
+  within_group_diversity$mean_group_centr_dist[within_group_diversity[, tax_level] == taxon] <- mean(
+    within_group_diversity$group_centr_dist[within_group_diversity[, tax_level] == taxon]
+    )
+  
+  # Calculate mean distance to within-group centroid and add to summary dataframe
+  summary_within_group_diversity$mean_group_centr_dist[summary_within_group_diversity[, tax_level] == taxon] <- mean(
+    within_group_diversity$group_centr_dist[within_group_diversity[, tax_level] == taxon]
+    )
+  
+  
+  
+  # Calculate mean within-group pairwise distance and append to main dataframe
+  within_group_diversity$mean_pairwise_dist[within_group_diversity[, tax_level] == taxon] <- mean(
+      dispRity::dispRity(
+      mat, metric = dispRity::pairwise.dist
+    )$disparity[[1]][[1]]
+  )
+  
+  # Add to summary dataframe
+  summary_within_group_diversity$mean_pairwise_dist[summary_within_group_diversity[, tax_level] == taxon] <-   within_group_diversity$mean_pairwise_dist[within_group_diversity[, tax_level] == taxon][1]
+  
+  
+}
+
+
+## Plotting on phylogeny
+
+# Select one tree for plotting purposes
+tree <- phy[[1]]
+
+# Get only first row of each taxonomic family/order/tax subgroup
+phylo_dat <- within_group_diversity %>% 
+  group_by(get(tax_level)) %>% 
+  filter(row_number() == 1) %>% 
+  as.data.frame() %>% 
+  select(
+    -ncol(.)
+  )
+rownames(phylo_dat) <- phylo_dat$species
+
+# Filter tree to one species per taxon
+tree <- ape::drop.tip(tree, tip = setdiff(tree$tip.label, rownames(phylo_dat)))
+
+# Reorder diversity data to match phylogeny
+phylo_dat <- phylo_dat[match(tree$tip.label, rownames(phylo_dat)), , drop = F]
+# Verify identical
+identical(rownames(phylo_dat), tree$tip.label)
+
+# Overwrite species column and rownames as taxon
+phylo_dat$species <- phylo_dat[, tax_level]
+rownames(phylo_dat) <- phylo_dat[, tax_level]
+
+# Overwrite tree tips with taxon only
+tree$tip.label <- phylo_dat[, tax_level]
+
+# Get tree trait tip data
+td <- data.frame(node = tidytree::nodeid(tree, rownames(phylo_dat)),
+                 mean_group_centr_dist = phylo_dat[, "mean_group_centr_dist"],
+                 mean_pairwise_dist = phylo_dat[, "mean_pairwise_dist"])
+td$node <- as.numeric(td$node)
+
+# put data and tree together
+phylo_dat_tree <- full_join(tree, td, by = "node")
+rm(td)
+
+## Plot diversity statistics on temrinal branches and tips
+# set plotting parameters
+if(tax_level == "species"){
+  branch_width <- 0.35
+  text_size <- 0.55
+  line_width <- 0.2
+  tip_size <- 1
+  folder <- "species_level"
+} else if (tax_level == "genus"){
+  branch_width <- 0.35
+  text_size <- 0.65
+  line_width <- 0.3
+  tip_size <- 1
+  folder <- "genus_level"
+} else if (tax_level == "family"){
+  branch_width <- 0.45
+  text_size <- 1.8
+  line_width <- 0.7
+  tip_size <- 1
+  folder <- "family_level"
+} else if (tax_level == "order"){
+  branch_width <- 0.35
+  text_size <- 2
+  line_width <- 1
+  tip_size <- 1
+  folder <- "order_level"
+} else if (tax_level == "taxon_subgroup"){
+  branch_width <- 0.7
+  text_size <- 2
+  line_width <- 2
+  tip_size <- 4
+  folder <- "taxon_subgroup_level"
+}
+
+# Mean distance to group centroid
+if(log_choice == TRUE) {
+  p <- ggtree::ggtree(phylo_dat_tree, aes(colour = log(mean_group_centr_dist)),
+                    layout = "circular",
+                    ladderize = TRUE, continuous = "colour", size = branch_width) + 
+  geom_tippoint(aes(colour = log(mean_group_centr_dist)), size = tip_size) + 
+  geom_tiplab(aes(colour = log(mean_group_centr_dist)), offset = 3, size = text_size) +
+  scale_color_viridis_c(option = "D")
+} else {
+  p <- ggtree::ggtree(phylo_dat_tree, aes(colour = mean_group_centr_dist),
+                      layout = "circular",
+                      ladderize = TRUE, continuous = "colour", size = branch_width) + 
+    geom_tippoint(aes(colour = mean_group_centr_dist), size = tip_size) + 
+    geom_tiplab(aes(colour = mean_group_centr_dist), offset = 3, size = text_size) +
+    scale_color_viridis_c(option = "D")
+  }
+p
+
+# Save as SVG
+if(log_choice == TRUE) {
+  log_filename <- "LOG_"
+} else {
+  log_filename <- ""
+}
+
+svg(
+  here::here(
+    "2_Patches", "4_OutputPlots", "2_Diversity_measures", "2_Diversity_phylogeny",
+    "within_group_diversity", 
+    paste0("patch_jndxyzlum_", log_filename, "mean_group_centr_dist", "_", sex_choice, "_", "phylogeny.svg")
+  ), 
+  width = 6, height = 6
+)
+
+p
+
+dev.off()
+
+# Mean within-group pairwise distance
+if(log_choice == TRUE) {
+  p <- ggtree::ggtree(phylo_dat_tree, aes(colour = log(mean_pairwise_dist)),
+                      layout = "circular",
+                      ladderize = TRUE, continuous = "colour", size = branch_width) + 
+    geom_tippoint(aes(colour = log(mean_pairwise_dist)), size = tip_size) + 
+    geom_tiplab(aes(colour = log(mean_pairwise_dist)), offset = 3, size = text_size) +
+    scale_color_viridis_c(option = "D")
+} else {
+  p <- ggtree::ggtree(phylo_dat_tree, aes(colour = mean_pairwise_dist),
+                      layout = "circular",
+                      ladderize = TRUE, continuous = "colour", size = branch_width) + 
+    geom_tippoint(aes(colour = mean_pairwise_dist), size = tip_size) + 
+    geom_tiplab(aes(colour = mean_pairwise_dist), offset = 3, size = text_size) +
+    scale_color_viridis_c(option = "D")
+}
+p
+
+# Save as SVG
+if(log_choice == TRUE) {
+  log_filename <- "LOG_"
+} else {
+  log_filename <- ""
+}
+
+svg(
+  here::here(
+    "2_Patches", "4_OutputPlots", "2_Diversity_measures", "2_Diversity_phylogeny",
+    "within_group_diversity", 
+    paste0("patch_jndxyzlum_", log_filename, "mean_pairwise_dist", "_", sex_choice, "_", "phylogeny.svg")
+  ), 
+  width = 6, height = 6
+)
+
+p
+
+dev.off()
