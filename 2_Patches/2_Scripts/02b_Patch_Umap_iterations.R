be93e5e6ede333c602f385a13c32e641178a58fc
MacRob-X <robert-macdonald@live.co.uk>
2024-11-22 12:00:15 +0000

    Randomise UMAP seed
    Set UMAP iterations to not preserve seed to give truly random initialisation. Also add parameterisation for ease of use

--- 2_Patches/2_Scripts/02b_Patch_Umap_iterations.R
+++ 2_Patches/2_Scripts/02b_Patch_Umap_iterations.R
@@ -5,8 +5,6 @@
 # clear environment
 rm(list=ls())
 
-# load libraries
-library(umap)
 
 # Custom functions ----
 source(
@@ -15,16 +13,31 @@ source(
   )
 )
 
-# load patch PCA data - specify e.g. jndxyzlum
-pca_all <- readr::read_rds(
+## EDITABLE CODE ##
+# Select subset of species ("Neoaves" or "Passeriformes")
+clade <- "Passeriformes"
+# select type of colour pattern space to use ("jndxyzlum", "usmldbl", "usmldblr")
+# N.B. will need to change the date in the pca_all filename if using usmldbl or usmldblr
+# (from 240603 to 240806)
+space <- "usml"
+# select sex ("M", "F", "All")
+sex <- "F"
+# select UMAP parameters
+nn <- "default"
+min_dist <- "default"
+## END EDITABLE CODE
+
+# load patch data (PCA of whichever colourspace - generated in 02_Patch_Analyse_features.R)
+pca_filename <- paste(clade, "patches.231030.PCAcolspaces", space, "240925", "rds", sep = ".")
+pca_all <- readRDS(
   here::here(
-    "2_Patches", "3_OutputData", "2_PCA_ColourPattern_spaces", "1_Raw_PCA", 
-    "Neoaves.patches.231030.PCAcolspaces.jndxyzlum.240603.rds"
+    "2_Patches", "3_OutputData", "2_PCA_ColourPattern_spaces", "1_Raw_PCA",
+    pca_filename
   )
 )
 
 # load taxonomic data
-taxo <- readr::read_csv(
+taxo <- read.csv(
   here::here(
     "4_SharedInputData", "BLIOCPhyloMasterTax_2019_10_28.csv"
    )
@@ -35,34 +48,39 @@ taxo <- readr::read_csv(
 
 # Perform UMAP four times with the default parameters to see if clustering is visually different 
 # Plot and save as png
-nUMAPs <- 4
-rawUMAPs <- vector("list", length = nUMAPs)
+n_umaps <- 4
+raw_umaps <- vector("list", length = n_umaps)
+date <- gsub("-", "", Sys.Date())
 
 # perform UMAPs
-for (i in 1:nUMAPs){
+for (i in 1:n_umaps){
   cat("\r", i)
-  rawUMAPs[[i]] <- umap(pca_all$x)
+  raw_umaps[[i]] <- umap::umap(pca_all$x, preserve.seed = FALSE)
 }
 
-saveRDS(rawUMAPs, 
+# save
+umap_filename <- paste(clade, "patches.pca", space, "UMAPs.iterations", date, "rds", sep = ".")
+saveRDS(raw_umaps, 
         file = here::here(
           "2_Patches", "3_OutputData", "2_PCA_ColourPattern_spaces", "2_UMAP", 
-          "Neoaves.patches.pca.jndxyzlum.UMAPs.iterations.240603.rds"
+          umap_filename
           )
         )
 
 # Trim and order taxonomic data to match UMAP data
+raw_umaps <- readRDS(file = here::here(
+  "2_Patches", "3_OutputData", "2_PCA_ColourPattern_spaces", "2_UMAP", "Passeriformes.patches.pca.usml.UMAPs.iterations.20240927.rds"))
 
 # create dataframe from UMAP to manipulate
-cleanUMAPs <- vector("list", length = nUMAPs)
-for (j in 1:nUMAPs){
-  cleanUMAPs[[j]] <- data.frame(rawUMAPs[[j]]$layout)
-  l <- length(rawUMAPs[[j]]$layout[,1])
+clean_umaps <- vector("list", length = n_umaps)
+for (j in 1:n_umaps){
+  clean_umaps[[j]] <- data.frame(raw_umaps[[j]]$layout)
+  l <- length(raw_umaps[[j]]$layout[,1])
   for(i in 1:l){
     cat("\r", i)
-    split <- strsplit(rownames(cleanUMAPs[[j]])[i], split = "-")[[1]]
-    cleanUMAPs[[j]]$species[i] <- split[1]
-    cleanUMAPs[[j]]$sex[i] <- split[2]
+    split <- strsplit(rownames(clean_umaps[[j]])[i], split = "-")[[1]]
+    clean_umaps[[j]]$species[i] <- split[1]
+    clean_umaps[[j]]$sex[i] <- split[2]
   }
 }
 
@@ -70,143 +88,149 @@ for (j in 1:nUMAPs){
 taxo$specimen <- c(rep(NA, times = length(taxo$TipLabel)))
 
 # get female specimen taxonomic data
-femTaxo <- taxo[paste0(taxo$TipLabel, "-F") %in% rownames(cleanUMAPs[[1]]), ]
-femTaxo$specimen <- paste0(femTaxo$TipLabel, "-F")
+fem_taxo <- taxo[paste0(taxo$TipLabel, "-F") %in% rownames(clean_umaps[[1]]), ]
+fem_taxo$specimen <- paste0(fem_taxo$TipLabel, "-F")
 
 # get male specimen taxonomic data
-maleTaxo <- taxo[paste0(taxo$TipLabel, "-M") %in% rownames(cleanUMAPs[[1]]), ]
-maleTaxo$specimen <- paste0(maleTaxo$TipLabel, "-M")
+male_taxo <- taxo[paste0(taxo$TipLabel, "-M") %in% rownames(clean_umaps[[1]]), ]
+male_taxo$specimen <- paste0(male_taxo$TipLabel, "-M")
 
 # get unknown sex specimen taxonomic data
-unkTaxo <- taxo[paste0(taxo$TipLabel, "-U") %in% rownames(cleanUMAPs[[1]]), ]
-unkTaxo$specimen <- paste0(unkTaxo$TipLabel, "-U")
+unk_taxo <- taxo[paste0(taxo$TipLabel, "-U") %in% rownames(clean_umaps[[1]]), ]
+unk_taxo$specimen <- paste0(unk_taxo$TipLabel, "-U")
 
 # concatenate into one big dataframe with specimen names that match umapDat rownames
-taxoMatch <- rbind(femTaxo, maleTaxo, unkTaxo)
+taxo_match <- rbind(fem_taxo, male_taxo, unk_taxo)
 
 # remove temp dataframes
-rm(femTaxo, maleTaxo, unkTaxo)
+rm(fem_taxo, male_taxo, unk_taxo)
 
 # remove species not present in taxonomic data
-cleanUMAPs <- lapply(cleanUMAPs, function(df){
-  df <- df[rownames(df) %in% taxoMatch$specimen,]
+clean_umaps <- lapply(clean_umaps, function(df){
+  df <- df[rownames(df) %in% taxo_match$specimen,]
   return(df)
 })
 
-# reorder taxo data to match umapDat
-taxoMatch <- taxoMatch[match(rownames(cleanUMAPs[[1]]), taxoMatch$specimen), ]
+# reorder taxo data to match umap date
+taxo_match <- taxo_match[match(rownames(clean_umaps[[1]]), taxo_match$specimen), ]
 
 # Check all specimens match between the two datasets
-if(identical(taxoMatch$specimen, rownames(cleanUMAPs[[1]]))){
+if(identical(taxo_match$specimen, rownames(clean_umaps[[1]]))){
   print("All specimens in taxonomic and colour data matched")
 }
 
 # attach taxon subgroups to UMAP data and rename columns
-cleanUMAPs <- lapply(cleanUMAPs, function(df){
+clean_umaps <- lapply(clean_umaps, function(df){
   colnames(df) <- c("UMAP_1", "UMAP_2")
-  df$taxon_subgroup <- taxoMatch$Taxon_subgroup
+  df$taxon_subgroup <- taxo_match$Taxon_subgroup
   return(df)
 })
 
 
 # plot the four UMAPs together, coloured by taxon subgroup
-plot.FourPatch(cleanUMAPs[[1]],
-               cleanUMAPs[[2]],
-               cleanUMAPs[[3]],
-               cleanUMAPs[[4]],
+plot_filename <- paste(clade, "patches.pca", space, "UMAPs.iterations", date, "png", sep = ".")
+plot.FourPatch(clean_umaps[[1]],
+               clean_umaps[[2]],
+               clean_umaps[[3]],
+               clean_umaps[[4]],
                taxonCol = TRUE,
                filepath = here::here(
                  "2_Patches", "4_OutputPlots", "1_Colourspace_visualisation", "umap_iterations",
-                 "Neoaves.patches.pca.jndxyzlum.UMAPiterations.240603.png"
+                 plot_filename
                  )
                )
 
 # check if identical
-identical(cleanUMAPs[[1]], cleanUMAPs[[2]])
-identical(cleanUMAPs[[2]], cleanUMAPs[[3]])
-identical(cleanUMAPs[[3]], cleanUMAPs[[4]])
+identical(clean_umaps[[1]], clean_umaps[[2]])
+identical(clean_umaps[[2]], clean_umaps[[3]])
+identical(clean_umaps[[3]], clean_umaps[[4]])
 
 #--------------------------------------------------------------------------------#
 # Perform UMAP 8 times with different parameters to visually check if clustering is affected
-
+date <- gsub("-", "", Sys.Date())
 # set n_neighbours (based on Alam et al., 2024, Nature)
 # default is 15
 # note that runs with nn > 100 take a long time to run ( ~30 mins for nn = 200)
-customConfig <- umap.defaults
+custom_config <- umap::umap.defaults
 nn <- c(25, 35, 45, 55)
-nnUMAPs <- vector("list", length = length(nn))
+nn_umaps <- vector("list", length = length(nn))
 for (i in 1:length(nn)){
   cat("\r", i)
   # set custom configuration
-  customConfig$n_neighbors <- nn[i]
-  nnUMAPs[[i]] <- umap(pca_all$x, config = customConfig)
+  custom_config$n_neighbors <- nn[i]
+  nn_umaps[[i]] <- umap::umap(pca_all$x, config = custom_config)
 }
 
-saveRDS(nnUMAPs, 
+# save
+umap_filename <- paste(clade, "patches.pca", space, "nnUMAPs", date, "rds", sep = ".")
+saveRDS(nn_umaps, 
         file = here::here(
           "2_Patches", "3_OutputData", "2_PCA_ColourPattern_spaces", "2_UMAP", 
-          "Neoaves.patches.pca.jndxyzlum.nnUMAPs.25.35.45.55.240603.rds"
+          umap_filename
         )
       )
 
 # create list of dataframes from UMAPs
-nncleanUMAPs <- lapply(nnUMAPs, function(df){
+nnclean_umaps <- lapply(nn_umaps, function(df){
   df <- data.frame(df$layout)
   colnames(df) <- c("UMAP_1", "UMAP_2")
-  df$taxon_subgroup <- taxoMatch$Taxon_subgroup
+  df$taxon_subgroup <- taxo_match$Taxon_subgroup
   return(df)
 })
 
 
 # plot the four UMAPs together, coloured by taxon subgroup
-plot.FourPatch(nncleanUMAPs[[1]],
-               nncleanUMAPs[[2]],
-               nncleanUMAPs[[3]],
-               nncleanUMAPs[[4]],
+plot_filename <- paste(clade, "patches.pca", space, "nnUMAPs", date, "png", sep = ".")
+plot.FourPatch(nnclean_umaps[[1]],
+               nnclean_umaps[[2]],
+               nnclean_umaps[[3]],
+               nnclean_umaps[[4]],
                taxonCol = T,
                filepath = here::here(
                  "2_Patches", "4_OutputPlots", "1_Colourspace_visualisation", "umap_iterations",
-                 "Neoaves.patches.pca.jndxyzlum.nnUMAPs.25.35.45.55.240603.png"
+                 plot_filename
                )
 )
 
 
 # set minimum distance (again based on Alam et al., 2024, Nature)
 # default is 0.1
-customConfig <- umap.defaults
-minDist <- c(0.1, 0.25, 0.5, 0.99)
-minDistUMAPs <- vector("list", length = length(minDist))
-for (i in 1:length(minDist)){
+custom_config <- umap::umap.defaults
+min_dist <- c(0.1, 0.25, 0.5, 0.99)
+min_dist_umaps <- vector("list", length = length(min_dist))
+for (i in 1:length(min_dist)){
   cat("\r", i)
   # set custom configuration
-  customConfig$min_dist <- minDist[i]
-  minDistUMAPs[[i]] <- umap(pca_all$x, config = customConfig)
+  custom_config$min_dist <- min_dist[i]
+  min_dist_umaps[[i]] <- umap::umap(pca_all$x, config = custom_config)
 }
 
-saveRDS(minDistUMAPs, 
+umap_filename <- paste(clade, "patches.pca", space, "minDistUMAPs", date, "rds", sep = ".")
+saveRDS(min_dist_umaps, 
         file = here::here(
           "2_Patches", "3_OutputData", "2_PCA_ColourPattern_spaces", "2_UMAP", 
-          "Neoaves.patches.pca.jndxyzlum.minDistUMAPs.240604.rds"
+          plot_filename
         )
       )
 
 # create list of dataframes from UMAPs
-mindistcleanUMAPs <- lapply(minDistUMAPs, function(df){
+min_dist_clean_umaps <- lapply(min_dist_umaps, function(df){
   df <- data.frame(df$layout)
   colnames(df) <- c("UMAP_1", "UMAP_2")
-  df$taxon_subgroup <- taxoMatch$Taxon_subgroup
+  df$taxon_subgroup <- taxo_match$Taxon_subgroup
   return(df)
 })
 
 # plot the four UMAPs together, coloured by taxon subgroup
-plot.FourPatch(mindistcleanUMAPs[[1]],
-               mindistcleanUMAPs[[2]],
-               mindistcleanUMAPs[[3]],
-               mindistcleanUMAPs[[4]],
+plot_filename <- paste(clade, "patches.pca", space, "minDistUMAPs", date, "png", sep = ".")
+plot.FourPatch(min_dist_clean_umaps[[1]],
+               min_dist_clean_umaps[[2]],
+               min_dist_clean_umaps[[3]],
+               min_dist_clean_umaps[[4]],
                taxonCol = T,
                filepath = here::here(
                  "2_Patches", "4_OutputPlots", "1_Colourspace_visualisation", "umap_iterations",
-                 "Neoaves.patches.pca.jndxyzlum.minDistUMAPs.1.25.50.99.240603.png"
+                 plot_filename
                )
               )
 
@@ -265,17 +289,17 @@ spotCheckUMAP <- function(umap, type, lower1, upper1, lower2, upper2, showValues
 
 
                     
-spotCheckUMAP(nncleanUMAPs[[1]], type = "range", lower1 = -5, upper1 = 5, lower2 = -5, upper2 = 5)        
+spotCheckUMAP(nnclean_umaps[[1]], type = "range", lower1 = -5, upper1 = 5, lower2 = -5, upper2 = 5)        
 
 
 
 
 
-cols <- rainbow(length(levels(as.factor(nncleanUMAPs[[1]]$taxon_subgroup))))
-col_mapping <- cols[match(nncleanUMAPs[[1]]$taxon_subgroup, levels(as.factor(nncleanUMAPs[[1]]$taxon_subgroup)))]
+cols <- rainbow(length(levels(as.factor(nnclean_umaps[[1]]$taxon_subgroup))))
+col_mapping <- cols[match(nnclean_umaps[[1]]$taxon_subgroup, levels(as.factor(nnclean_umaps[[1]]$taxon_subgroup)))]
 
 
-plot(nncleanUMAPs[[1]][,2] ~ nncleanUMAPs[[1]][,1],
+plot(nnclean_umaps[[1]][,2] ~ nnclean_umaps[[1]][,1],
      asp = T,
      xlab = "UMAP 1", ylab = "UMAP 2", 
      las = 1,
