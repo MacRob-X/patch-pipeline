398ccb831583a4b2d477e25a2dfb617f4bfa3e37
MacRob-X <robert-macdonald@live.co.uk>
2025-06-13 17:37:04 +0100

    IUCN cat percentage coverage and significance test

--- 2_Patches/2_Scripts/00_Patch_number_specimens.R
+++ 2_Patches/2_Scripts/00_Patch_number_specimens.R
@@ -132,3 +132,68 @@ n_neoaves_genera_matchedsex <- px %>%
   length()
 
 (n_neoaves_genera_matchedsex / n_neoaves_genera_jetz) * 100
+
+
+# Quantify percentage species coverage of IUCN categories ----
+
+# select IUCN type
+iucn_type <- "nominate"
+
+# load IUCN data
+iucn_filename <- paste0("iucn_2024_", iucn_type, ".csv")
+iucn <- read.csv(
+  here::here(
+    "4_SharedInputData", iucn_filename
+  )
+)[, c("species_birdtree", "iucn_cat")] %>% 
+  rename(
+    species = species_birdtree
+  ) %>% 
+  filter(
+    !(iucn_cat %in% c("DD", "EW", "EX"))
+  )
+
+# join to matched sex species
+matched_iucn <- male_species %>% 
+  inner_join(
+    female_species
+  ) %>% 
+  left_join(
+    iucn, by = "species"
+  )
+
+# get taxonomy with iucn categories (filtered to Neoaves)
+taxo_iucn <- taxo %>% 
+  filter(
+    IOCOrder != "STRUTHIONIFORMES", 
+    IOCOrder != "RHEIFORMES", 
+    IOCOrder != "CASUARIIFORMES", 
+    IOCOrder != "APTERYGIFORMES", 
+    IOCOrder != "TINAMIFORMES", 
+    IOCOrder != "GALLIFORMES", 
+    IOCOrder != "ANSERIFORMES"
+  ) %>% 
+  left_join(
+    iucn, by = join_by("TipLabel" == "species")
+  )
+
+# filter to each IUCN category and print percentage coverage
+for(category in c("CR", "EN", "VU", "NT", "LC")){
+  
+  n_taxo <- taxo_iucn %>% 
+    filter(
+      iucn_cat == category
+    ) %>% 
+    nrow()
+  
+  n_px <- matched_iucn %>% 
+    filter(
+      iucn_cat == category
+    ) %>% 
+    nrow()
+  
+  pc_cov <- (n_px / n_taxo) * 100
+  
+  print(paste0("Species coverage = ", round(pc_cov, digits = 1), "% for IUCN category ", category))
+  
+}
